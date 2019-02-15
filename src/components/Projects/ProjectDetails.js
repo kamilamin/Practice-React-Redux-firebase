0844eece962913076563ef423444246ea9462ec4
kamilamin <kamil.amin@live.co.uk>
2019-02-16 02:32:23 +0500

    Updating project details routes with project ID

--- src/components/Projects/ProjectDetails.js
+++ src/components/Projects/ProjectDetails.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+    console.log(props.match.params)
+    const id = props.match.params.id
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-            <span className="card-title">Project Title</span>
+            <span className="card-title">Project Title - {id}</span>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae optio, incidunt nobis porro eligendi perspiciatis explicabo et, autem commodi placeat modi vel ea, id cupiditate laboriosam eius aperiam voluptatibus obcaecati.</p>
         </div>
         <div className="card-action gret lighten-4 grey-text">
