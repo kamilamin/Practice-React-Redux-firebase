4d7846adccbf1670216b4bce71ab23af84b1f1bd
kamilamin <kamil.amin@live.co.uk>
2019-02-16 02:22:30 +0500

    Project details route is setting up in project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Layout/NavBar";
 import Dashboard from "./components/Dashboard/Dashboard";
+import ProjectDetails from './components/Projects/ProjectDetails';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
         <div>
           <Navbar />
           <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/project/:id" component={ProjectDetails} />
           </Switch>
         </div>
       </BrowserRouter>
