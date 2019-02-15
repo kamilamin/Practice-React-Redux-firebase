796fa131831e2b011e697b06c38f06b8dd9fc765
kamilamin <kamil.amin@live.co.uk>
2019-02-16 02:16:15 +0500

    Setting up project details component in project

---
+++ src/components/Projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ProjectDetails = () => {
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+            <span className="card-title">Project Title</span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae optio, incidunt nobis porro eligendi perspiciatis explicabo et, autem commodi placeat modi vel ea, id cupiditate laboriosam eius aperiam voluptatibus obcaecati.</p>
+        </div>
+        <div className="card-action gret lighten-4 grey-text">
+            <div>Posted by Kamil amin</div>
+            <div>16th Feb, 2019, 2AM</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectDetails
