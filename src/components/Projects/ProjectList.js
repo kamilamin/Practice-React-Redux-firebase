075ff2f4a25a80be112cb2ff94b56af1a03dd0b0
kamilamin <kamil.amin@live.co.uk>
2019-02-16 00:13:07 +0500

    Fixing typo in project list component and setting up Project summery component

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -3,9 +3,9 @@ import React from "react";
 
 const ProjectList = () => {
     return (
-        <div classNameName="project-list section">
+        <div className="project-list section">
 
-            <div classNameName="card z-depth-0 project-summary">
+            <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">Project Title</span>
                     <p>Posted by Kamil amin</p>
@@ -13,7 +13,7 @@ const ProjectList = () => {
                 </div>
             </div>
 
-            <div classNameName="card z-depth-0 project-summary">
+            <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">Project Title</span>
                     <p>Posted by Kamil amin</p>
@@ -21,7 +21,7 @@ const ProjectList = () => {
                 </div>
             </div>
 
-            <div classNameName="card z-depth-0 project-summary">
+            <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">Project Title</span>
                     <p>Posted by Kamil amin</p>
@@ -29,7 +29,7 @@ const ProjectList = () => {
                 </div>
             </div>
 
-            <div classNameName="card z-depth-0 project-summary">
+            <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">Project Title</span>
                     <p>Posted by Kamil amin</p>
---
+++ src/components/Projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Kamil amin</p>
+                    <p className="grey-text">16 Feb, 12AM</p>
+                </div>
+            </div>
+    )
+}
+
+export default ProjectSummary
