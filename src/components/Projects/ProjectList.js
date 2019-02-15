dadc0f31640c2ac5075b0ce621dedfb0c576a9a1
kamilamin <kamil.amin@live.co.uk>
2019-02-16 00:20:33 +0500

    Importing Project Summary component in Project list

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,42 +1,10 @@
 import React from "react";
-
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Kamil amin</p>
-                    <p className="grey-text">16 Feb, 12AM</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Kamil amin</p>
-                    <p className="grey-text">16 Feb, 12AM</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Kamil amin</p>
-                    <p className="grey-text">16 Feb, 12AM</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Kamil amin</p>
-                    <p className="grey-text">16 Feb, 12AM</p>
-                </div>
-            </div>
-
+            <ProjectSummary />
         </div>
     )
 }
