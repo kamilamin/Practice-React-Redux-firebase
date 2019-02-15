fa1899a5b9e284f42a382cff5b0d3e380d534d45
kamilamin <kamil.amin@live.co.uk>
2019-02-16 00:11:25 +0500

    Updating root Router in project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import Navbar from './components/Layout/NavBar'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Layout/NavBar";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Navbar />
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path="/" component={Dashboard} />
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
