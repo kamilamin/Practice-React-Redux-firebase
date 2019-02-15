5699bb786751065d72476e05a2bc91992c44d228
kamilamin <kamil.amin@live.co.uk>
2019-02-15 23:58:50 +0500

    Setting up project list component and import in Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Notifications from './Notifications'
+import ProjectList from '../Projects/ProjectList';
+
 
 class Dashboard extends Component {
     render() {
         return (
             <div className="dashboard container">
                 <div className="row">
-                    <div className="col s12 m6"></div>
+                    <div className="col s12 m6">
+                        <ProjectList />
+                    </div>
                     <div class="col s12 m5 offset-m1">
                         <Notifications />
                     </div>
---
+++ src/components/Projects/ProjectList.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+
+const ProjectList = () => {
+    return (
+        <div classNameName="project-list section">
+
+            <div classNameName="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Kamil amin</p>
+                    <p className="grey-text">16 Feb, 12AM</p>
+                </div>
+            </div>
+
+            <div classNameName="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Kamil amin</p>
+                    <p className="grey-text">16 Feb, 12AM</p>
+                </div>
+            </div>
+
+            <div classNameName="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Kamil amin</p>
+                    <p className="grey-text">16 Feb, 12AM</p>
+                </div>
+            </div>
+
+            <div classNameName="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Kamil amin</p>
+                    <p className="grey-text">16 Feb, 12AM</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default projectList;
