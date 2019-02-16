504f20d44fe5de54e43f5fd6ca83e2f9592cf87e
kamilamin <kamil.amin@live.co.uk>
2019-02-16 20:21:34 +0500

    Adding heading of sign in in Signin screen

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,34 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class SignIn extends Component {
-    state = {
-
-    }
-    handleChange = (e) => {
-        console.log(e)
-    }
-    handleSubmit = (e) => {
-        console.log(e)
-    }
+  state = {};
+  handleChange = e => {
+    console.log(e);
+  };
+  handleSubmit = e => {
+    console.log(e);
+  };
   render() {
     return (
       <div className="container">
-          <form onSubmit={this.handleSubmit} className="white">
-            <div className="input-field">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" onChange={this.handleChange} />
-            </div>
-            <div className="input-field">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" onChange={this.handleChange} />
-            </div>
-            <div>
-                <button className="btn pink lighten-1 z-depth-0">Login</button>
-            </div>
-          </form>
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5>Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" onChange={this.handleChange} />
+          </div>
+          <div>
+            <button className="btn pink lighten-1 z-depth-0">Login</button>
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default SignIn
+export default SignIn;
