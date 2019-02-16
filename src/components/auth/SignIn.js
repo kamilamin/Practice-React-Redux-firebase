2d0fc7bd0ee4e037aca653e81924b467aff6acf5
kamilamin <kamil.amin@live.co.uk>
2019-02-16 20:17:10 +0500

    Typo fixed in Sign In screen

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -23,7 +23,7 @@ class SignIn extends Component {
                 <input type="password" id="password" onChange={this.handleChange} />
             </div>
             <div>
-                <button class="btn pink lighten-1 z-depth-0">Login</button>
+                <button className="btn pink lighten-1 z-depth-0">Login</button>
             </div>
           </form>
       </div>
